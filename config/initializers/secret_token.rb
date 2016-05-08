a5010452e09d8e5d1331a7b6312258a3f48b5f93
h-nagahama <h-nagahama@towatech.net>
2016-05-08 12:50:46 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MtSns::Application.config.secret_key_base = '09c0e50eb8b8c02250b911a2445dd039a6b1b379a9fc6308dac1af111c55d2b4d0acb508655abc5c9d96a9bba94d12e305aa9235e0c37dfb88d1c2d56a0ec07b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MtSns::Application.config.session_store :cookie_store, key: '_mt-sns_session'
